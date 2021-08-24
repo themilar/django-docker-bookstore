86704c4f3179f84b054be5a93b9a657b71874ef6
J <Joeydre69@gmail.com>
2021-08-24 01:34:42 +0100

    search functionality

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.db.models import Q
 from django.views.generic import ListView, DetailView
 
 from .models import Book
@@ -13,3 +14,14 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = Book
     login_url = 'account_login'
     permission_required = 'books.special status'
+
+
+class SearchListView(ListView):
+    # queryset = Book.objects.filter(Q(title__icontains='doom') | Q(title__icontains='aqua'))
+    template_name = 'books/search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        queryset = Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query))
+        return queryset
