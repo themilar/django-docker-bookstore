6b650f7e7d702e62f09b5239775c658c8bada53e
J <Joeydre69@gmail.com>
2021-09-23 17:10:38 +0100

    indexing

--- books/models.py
+++ books/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
+
 class Book(models.Model):
     id = models.UUIDField(
         primary_key=True,
@@ -12,19 +13,23 @@ class Book(models.Model):
     )
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=6,decimal_places=2)
-    cover = models.ImageField(upload_to='covers/',blank=True)
-    
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    cover = models.ImageField(upload_to='covers/', blank=True)
+
     class Meta:
+        indexes = [
+            models.Index(fields=['id'], name='id_index'),
+        ]
         permissions = [
-            ('special status','Can read all books'),
+            ('special status', 'Can read all books'),
         ]
 
     def __str__(self):
         return self.title
-    
+
     def get_absolute_url(self):
-        return reverse('book_detail',kwargs={'pk':self.id})
+        return reverse('book_detail', kwargs={'pk': self.id})
+
 
 class Review(models.Model):
     book = models.ForeignKey(
